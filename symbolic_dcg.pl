cbe86290fb046757bac45634a2f1121b0cc772b5
Leonardo Santos <leonardosantos@Leonardos-MacBook-Pro.local>
2018-12-03 17:35:53 +0000

    digits

--- symbolic_dcg.pl
+++ symbolic_dcg.pl
@@ -1,16 +1,31 @@
 digits(0)-->[zero].
+digits(0)-->['0'].
 digits(1)-->[one].
+digits(1)-->['1'].
 digits(2)-->[two].
+digits(2)-->['2'].
 digits(3)-->[three].
+digits(3)-->['3'].
 digits(4)-->[four].
+digits(4)-->['4'].
 digits(5)-->[five].
+digits(5)-->['5'].
 digits(6)-->[six].
+digits(6)-->['6'].
 digits(7)-->[seven].
+digits(7)-->['7'].
 digits(8)-->[eight].
+digits(8)-->['8'].
 digits(9)-->[nine].
+digits(9)-->['9'].
 digits(10)-->[ten].
+digits(10)-->['10'].
 digits(11)-->[eleven].
+digits(11)-->['11'].
 digits(12)-->[twelve].
+digits(12)-->['12'].
+
+
 
 % digits(R)-->[fifty],digits(X),write(R), R is 50 + X.
 
